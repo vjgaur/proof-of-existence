b18236e692421c4ac0c5b1666e8ca836244a0ccc
vjgaur <vijayendra.gaur@gmail.com>
2021-12-25 20:05:28 +0530

    added events in pallets

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -12,11 +12,29 @@ pub mod pallet {
     use frame_system::pallet_prelude::*;
     use sp_std::vec::Vec; // Step 3.1 will include this in `Cargo.toml`
 
-    #[pallet::config]  // <-- Step 2. code block will replace this.
+    /// Configure the pallet by specifying the parameters and types on which it depends.
+	#[pallet::config]
+	pub trait Config: frame_system::Config {
+    /// Because this pallet emits events, it depends on the runtime's definition of an event.
+    type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+	
+	}
     #[pallet::event]   // <-- Step 3. code block will replace this.
     #[pallet::error]   // <-- Step 4. code block will replace this.
-    #[pallet::pallet]
-    #[pallet::generate_store(pub(super) trait Store)]
+
+	// Pallets use events to inform users when important changes are made.
+	// Event documentation should end with an array that provides descriptive names for parameters.
+	// https://docs.substrate.io/v3/runtime/events-and-errors
+	#[pallet::event]
+	#[pallet::generate_deposit(pub(super) fn deposit_event)]
+	pub enum Event<T: Config> {
+		/// Event emitted when a proof has been claimed. [who, claim]
+		ClaimCreated(T::AccountId, Vec<u8>),
+		/// Event emitted when a claim is revoked by the owner. [who, claim]
+		ClaimRevoked(T::AccountId, Vec<u8>),
+	}
+    
+	#[pallet::generate_store(pub(super) trait Store)]
     #[pallet::generate_storage_info]
     pub struct Pallet<T>(_);
 
